274f48af48ca7210e7632000a4a5de428a62cce9
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-04 14:11:09 +0000

    Stopwatch

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stopwatch
+            
+            int time = 0;
+            int stops = int.Parse(Console.ReadLine());
+            int[] times = new int[stops];
+            for (int i = 0; i < stops; i++)
+            {
+                times[i] = int.Parse(Console.ReadLine());
+            }
+            if (times.Length > 1)
+            {
+                for (int i = 1; i < times.Length; i+= 2)
+                {
+                    time += times[i] - times[i-1];
+                }
+            }
+            if (stops % 2 == 0)
+            {
+                Console.WriteLine(time);
+            }
+            else
+            {
+                Console.WriteLine("still running");
+            }
+        }
+    }
+}
