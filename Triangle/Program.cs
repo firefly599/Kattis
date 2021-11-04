65c8aa0dc16dea6f39d46c409ed9ba0f5b6291ac
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-04 12:35:55 +0000

    Triangle

---
+++ Triangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Triangle Area
+            float[] lengths = Array.ConvertAll(Console.ReadLine().Split(" "), float.Parse);
+            float area = (lengths[0] * lengths[1]) / 2;
+            Console.WriteLine(area);
+        }
+    }
+}
