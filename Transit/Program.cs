585922c21127f05492666152bf0ab1c7d1812fc3
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-04 12:36:23 +0000

    Transit - Needs completing

---
+++ Transit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Transit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Transit Woes
+            int[] first = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int[] second = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int[] third = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int[] forth = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int time = 0;
+            // idk
+            for (int i = 0; i < first[2]; i++)
+            {
+                time += second[i];
+                if (time < forth[i])
+                {
+                    time += forth[i] - second[i];
+                }
+                else
+                {
+                    time += forth[i]*2 - time;
+                }
+                time += third;
+            }
+            if (time <= (first[1] - first[0]))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
