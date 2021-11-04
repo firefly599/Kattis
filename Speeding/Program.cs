e0db9f3a0f9ff34440980332c2c18d80dba377d5
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-04 15:33:38 +0000

    Speeding

---
+++ Speeding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Speeding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Speeding
+            int samples = int.Parse(Console.ReadLine());
+            float[] control = new float[] { 0, 0 };
+            float speed = 0;
+            int max = 0;
+            for (int i = 0; i < samples; i++)
+            {
+                float[] data = Array.ConvertAll(Console.ReadLine().Split(" "), float.Parse);
+                if (i != 0)
+                {
+                    speed = ((data[1] - control[1]) / (data[0] - control[0]));
+                    if (speed > max)
+                    {
+                        max = (int)speed;
+                    }
+                }
+                control = data;
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
