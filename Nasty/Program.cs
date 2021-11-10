87b49791fe8784af2f39ff5657297b7151641b0f
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-10 00:07:00 +0000

    Nasty Hacks

---
+++ Nasty/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nasty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nasty Hacks
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int[] values = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if (values[0] < values[1] - values[2])
+                {
+                    Console.WriteLine("advertise");
+                }
+                else if (values[0] == values[1] - values[2])
+                {
+                    Console.WriteLine("does not matter");
+                }
+                else
+                {
+                    Console.WriteLine("do not advertise");
+                }
+            }
+        }
+    }
+}
