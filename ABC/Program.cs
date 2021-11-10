1b52cb2b5eab4a1dd0e6845c69f7cce46f9948f7
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-10 16:24:33 +0000

    ABC

---
+++ ABC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ABC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ABC
+            int[] values = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            Array.Sort(values);
+            string order = Console.ReadLine();
+            for (int i = 0; i < 3; i++)
+            {
+                if (order[i] == 'A')
+                {
+                    Console.Write(values[0] + " ");
+                }
+                else if (order[i] == 'B')
+                {
+                    Console.Write(values[1] + " ");
+                }
+                else
+                {
+                    Console.Write(values[2] + " ");
+                }
+            }
+        }
+    }
+}
