9b9ec21a667817bbcc5c89e340dc8d946efdc045
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-10 15:06:05 +0000

    The Key to Cryptography - Failed

---
+++ Cryptography/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Key to Cryptography
+            string encyp = Console.ReadLine();
+            string key = Console.ReadLine();
+            string combined = key + encyp;
+            combined = combined.Substring(0, encyp.Length);
+            int shift;
+            int value;
+            string decyp = "";
+            for (int i = 0; i < encyp.Length; i++)
+            {
+                decyp = (char) 
+            }
+            Console.WriteLine(decyp);
+        }
+    }
+}
