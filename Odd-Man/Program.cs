6bb48fc9241fd84994461cf3c2c226b4fbc8db49
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-10 17:05:03 +0000

    Odd Man Out

---
+++ Odd-Man/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Odd_Man
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Odd Man Out
+            int tests = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= tests; i++)
+            {
+                int guests = int.Parse(Console.ReadLine());
+                int[] numbers = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                for (int j = 0; j < guests; j++)
+                {
+                    bool matching = false;
+                    for (int k = 0; k < guests; k++)
+                    {
+                        if (j != k)
+                        {
+                            if (numbers[j] == numbers[k])
+                            {
+                                matching = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!matching)
+                    {
+                        Console.WriteLine("Case #{0}: {1}", i, numbers[j]);
+                    }
+                }
+            }
+        }
+    }
+}
