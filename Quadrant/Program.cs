da2a17b44957985904adfcca049c847e8e572f88
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-10 16:55:24 +0000

    Quadrant Selection

---
+++ Quadrant/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Quadrant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Quadrant Selection
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a > 0 && b > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (a > 0 && b < 0)
+            {
+                Console.WriteLine("4");
+            }
+            else if (a < 0 && b < 0)
+            {
+                Console.WriteLine("3");
+            }
+            else
+            {
+                Console.WriteLine("2");
+            }
+        }
+    }
+}
