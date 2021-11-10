2f137ddbb6b8dd5e13a9a986228f7aff1e3a18d3
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-10 00:13:07 +0000

    Spavanac

---
+++ Spavanac/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Spavanac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Spavanac
+            int[] time = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            time[1] -= 45;
+            if (time[1] < 0)
+            {
+                time[1] = 60 + time[1];
+                time[0]--;
+                if (time[0] < 0)
+                {
+                    time[0] = 23;
+                }
+            }
+            Console.WriteLine(time[0] + " " + time[1]);
+        }
+    }
+}
