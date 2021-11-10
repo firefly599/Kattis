2133302a9cd2e364f85630360aaafc169991139f
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-10 13:54:32 +0000

    Yin and Yang Stones

---
+++ Yin/Program.cs
@@ -0,0 +1,34 @@
+﻿    using System;
+
+namespace Yin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yin and Yang Stones
+            string input = Console.ReadLine();
+            int black = 0;
+            int white = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'W')
+                {
+                    white++;
+                }
+                else
+                {
+                    black++;
+                }
+            }
+            if (white == black)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
