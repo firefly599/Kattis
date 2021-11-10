8677620fef6dcec943f45cdae6fecc762bee69fc
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-10 17:10:54 +0000

    Mixed Fractions

---
+++ Fractions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mixed Fractions
+            int[] values = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int whole;
+            int remainder;
+            while (values[0] != 0 && values[1] != 0)
+            {
+                
+                whole = values[0] / values[1];
+                remainder = values[0] % values[1];
+                Console.WriteLine("{0} {1} / {2}", whole, remainder, values[1]);
+                values = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            }
+        }
+    }
+}
