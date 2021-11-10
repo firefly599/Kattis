e41531464087be25cfa987e61df5e335cede374a
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-10 14:20:27 +0000

    Tri

---
+++ Tri/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Tri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tri
+            int[] values = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            if ((values[0] + values[1]) == values[2])
+            {
+                Console.WriteLine(values[0] + "+" + values[1] + "=" + values[2]);
+            }
+            else if ((values[0] - values[1]) == values[2])
+            {
+                Console.WriteLine(values[0] + "-" + values[1] + "=" + values[2]);
+            }
+            else if ((values[0] * values[1]) == values[2])
+            {
+                Console.WriteLine(values[0] + "*" + values[1] + "=" + values[2]);
+            }
+            else if ((values[0] / values[1]) == values[2])
+            {
+                Console.WriteLine(values[0] + "/" + values[1] + "=" + values[2]);
+            }
+            else if (values[0] == (values[1] + values[2]))
+            {
+                Console.WriteLine(values[0] + "=" + values[1] + "+" + values[2]);
+            }
+            else if (values[0] == (values[1] - values[2]))
+            {
+                Console.WriteLine(values[0] + "=" + values[1] + "-" + values[2]);
+            }
+            else if (values[0] == (values[1] * values[2]))
+            {
+                Console.WriteLine(values[0] + "=" + values[1] + "*" + values[2]);
+            }
+            else if (values[0] == (values[1] / values[2]))
+            {
+                Console.WriteLine(values[0] + "=" + values[1] + "/" + values[2]);
+            }
+
+        }
+    }
+}
