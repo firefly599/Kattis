d68dd5db84d4de69e4af19819c9e596ced03911f
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-12 20:54:03 +0000

    Booking a Room

---
+++ Booking/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Booking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Booking a Room
+            int[] rooms = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            List<int> booked = new List<int>();
+            int room;
+            for (int i = 0; i < rooms[1]; i++)
+            {
+                room = int.Parse(Console.ReadLine());
+                booked.Add(room);
+            }
+            booked.Sort();
+            bool filled = true;
+            for (int i = 1; i <= rooms[0]; i++)
+            {
+                if (!(booked.Contains(i)))
+                {
+                    filled = false;
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            if (filled)
+            {
+                Console.WriteLine("too late");
+            }
+        }
+    }
+}
