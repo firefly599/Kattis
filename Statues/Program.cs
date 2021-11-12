33bd432bb0fc125d6bba9dc39310c1399bb8c0f4
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-12 19:19:27 +0000

    3D Printed Statues

---
+++ Statues/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Statues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3D Printed Statues
+            int statues = int.Parse(Console.ReadLine());
+            int days = 1 + (int)Math.Ceiling(Math.Log(statues) / Math.Log(2));
+            Console.WriteLine(days);
+        }
+    }
+}
