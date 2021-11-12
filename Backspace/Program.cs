6a24be981e685f1736d5393eb6b1012e0c851909
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-12 18:28:48 +0000

    Backspace -- Time limi exceeded

---
+++ Backspace/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Backspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Backspace
+            string input = Console.ReadLine();
+            string copy = input;
+            int amount = copy.Split('<').Length - 1;
+
+            int index;
+            for (int i = 0; i < amount; i++)
+            {
+                index = input.IndexOf('<') - 1;
+                input = input.Remove(index, 2);
+            }
+            Console.WriteLine(input);
+            
+        }
+    }
+}
