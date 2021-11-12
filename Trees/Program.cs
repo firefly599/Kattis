e3e20c31b4c55e43e956dcb13c14a2f96e3e1e69
firefly499 <nikolaivalkamo1@gmail.com>
2021-11-12 00:28:33 +0000

    Planting Trees

---
+++ Trees/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Planting Trees
+            int trees = int.Parse(Console.ReadLine());
+            int[] days = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int minimum = 0;
+            Array.Sort(days);
+            Array.Reverse(days);
+            for (int i = 1; i <= trees; i++)
+            {
+                if (i + days[i-1] > minimum)
+                {
+                    minimum = days[i-1] + i;
+                }
+            }
+            Console.WriteLine(minimum + 1);
+        }
+    }
+}
