781161ee06c2c0dd8be27154ece8c0185d5e7ef0
firefly599 <nikolaivalkamo1@gmail.com>
2021-11-22 16:57:57 +0000

    Rgged Right

---
+++ Ragged/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ragged
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Rgged Right
+            string input;
+            List<string> lines = new List<string>();
+            int max = 0;
+            double score = 0;
+            int length;
+            while(!string.IsNullOrEmpty(input = Console.ReadLine()))
+            {
+                lines.Add(input);
+                length = input.Length;
+                if (length > max)
+                {
+                    max = length;
+                }
+            }
+            for (int i = 0; i < lines.Count - 1; i++)
+            {
+                length = lines[i].Length;
+                score += Math.Pow((max - length), 2);
+            }
+            Console.WriteLine(score);
+        }
+    }
+}
