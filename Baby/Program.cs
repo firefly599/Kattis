3a10e4e61d3d37974fc3a028693fec9948e3ece3
firefly599 <nikolaivalkamo1@gmail.com>
2021-11-22 16:57:46 +0000

    Baby Bites

---
+++ Baby/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace baby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Baby Bites
+            int numbers = int.Parse(Console.ReadLine());
+            string[] named = Console.ReadLine().Split(" ");
+            bool correct = true;
+            int counter = 1;
+            for (int i = 0; i < numbers; i++)
+            {
+               
+                if (int.TryParse(named[i], out int current))
+                {
+
+                    if (!(counter == current))
+                    {
+                        correct = false;
+                    }
+                }
+                counter++;
+            }
+            if (correct)
+            {
+                Console.WriteLine("makes sense");
+            }
+            else
+            {
+                Console.WriteLine("something is fishy");
+            }
+        }
+    }
+}
