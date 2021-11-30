481983a907a49fce56e5bcf1532af4b82c08fa5a
firefly599 <nikolaivalkamo1@gmail.com>
2021-11-30 15:01:16 +0000

    IsItHalloween.com

---
+++ Halloween/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Halloween
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IsItHalloween.com
+            string date = Console.ReadLine();
+            if (date == "OCT 31" || date == "DEC 25")
+            {
+                Console.WriteLine("yup");
+            }
+            else
+            {
+                Console.WriteLine("nope");
+            }
+        }
+    }
+}
