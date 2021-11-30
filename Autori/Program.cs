076c8ff4def286d6d91747a40c5559b033a46019
firefly599 <nikolaivalkamo1@gmail.com>
2021-11-30 15:07:06 +0000

    Autori

---
+++ Autori/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Autori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Autori
+            string[] name = Console.ReadLine().Split("-");
+            string initials = "";
+            foreach (string sub in name)
+            {
+                initials += sub[0];
+            }
+            Console.WriteLine(initials);
+        }
+    }
+}
