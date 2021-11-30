62fefaef413074fc14fdca52f5bbcf3e47f4abe9
firefly599 <nikolaivalkamo1@gmail.com>
2021-11-30 15:06:59 +0000

    R2

---
+++ R2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace R2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // R2
+            int[] values = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int target = values[1] * 2;
+            Console.WriteLine(target - values[0]);
+        }
+    }
+}
