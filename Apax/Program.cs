13cea3b592342ada545cefa546aee03f5953636a
firefly599 <nikolaivalkamo1@gmail.com>
2021-11-30 15:25:50 +0000

    Apaxiaaaaaaaaaaaans!

---
+++ Apax/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Apax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Apaxiaaaaaaaaaaaans!
+            string word = Console.ReadLine();
+            List<char> converted = new List<char>();
+            converted.Add(word[0]);
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (word[i] != word[i - 1])
+                {
+                    converted.Add(word[i]);
+                }
+            }
+            Console.WriteLine(new string(converted.ToArray()));
+        }
+    }
+}
