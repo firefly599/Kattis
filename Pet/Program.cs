f96900238c5a7773b7114b285c6ded22bad182d7
firefly599 <nikolaivalkamo1@gmail.com>
2021-11-30 15:01:24 +0000

    Pet

---
+++ Pet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pet
+            int highest = 0;
+            int pos = 0;
+            int sum = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                int[] line = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                sum = 0;
+                foreach(int value in line)
+                {
+                    sum += value;
+                }
+                if (sum > highest)
+                {
+                    highest = sum;
+                    pos = i;
+                }
+            }
+            Console.WriteLine("{0} {1}", pos, highest);
+        }
+    }
+}
