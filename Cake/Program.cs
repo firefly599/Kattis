fe7cc005b82cbabc26c698211314b03c651fc31b
firefly599 <nikolaivalkamo1@gmail.com>
2021-12-15 10:14:21 +0000

    Shattered cake

---
+++ Cake/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shattered Cake
+            int width = int.Parse(Console.ReadLine());
+            int pieces = int.Parse(Console.ReadLine());
+            int size = 0;
+            for (int i = 0; i < pieces; i++)
+            {
+                int[] dimensions = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                size += (dimensions[0] * dimensions[1]);
+            }
+            Console.WriteLine(size/width);
+        }
+    }
+}
