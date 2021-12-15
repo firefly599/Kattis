7a047be4b496c5b259b93bce1f0a85897139cc8a
firefly599 <nikolaivalkamo1@gmail.com>
2021-12-15 10:22:41 +0000

    Roaming Roman

---
+++ Roman/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace roman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Roaming Romans
+            float distance = float.Parse(Console.ReadLine());
+            double dis = Math.Round((1000f * (5280f/4854f)) * distance);
+            Console.WriteLine(dis);
+        }
+    }
+}
