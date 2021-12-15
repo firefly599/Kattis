a640a501ed346142516b81e9d2a514cd117fb4d0
firefly599 <nikolaivalkamo1@gmail.com>
2021-12-15 10:46:25 +0000

    Reversed Binary Numbers

---
+++ numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reversed Binary Numbers
+            int num = int.Parse(Console.ReadLine());
+            char[] binary = Convert.ToString(num, 2).ToCharArray();
+            Array.Reverse(binary);
+            string reversed = new string(binary);
+            Console.WriteLine(Convert.ToInt32(reversed, 2));
+        }
+    }
+}
