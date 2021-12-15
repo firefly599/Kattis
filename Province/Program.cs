b6f35244d7e01e358be9b2197493e29923eb4a59
firefly599 <nikolaivalkamo1@gmail.com>
2021-12-15 10:46:57 +0000

    Provinces and Gold

---
+++ Province/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Province
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Provinces and Gold
+            int[] coins = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int power = coins[0] * 3 + coins[1] * 2 + coins[2];
+            string buy = "";
+            if (power >= 8)
+            {
+                buy = "Province";
+            }
+            else if (power >= 5)
+            {
+                buy = "Duchy";
+            }
+            else if (power >= 2)
+            {
+                buy = "Estate";
+            }
+
+            if (buy.Length > 1)
+            {
+                buy += " or ";
+            }
+
+            if (power >= 6)
+            {
+                buy += "Gold";
+            }
+            else if (power >= 3)
+            {
+                buy += "Silver";
+            }
+            else
+            {
+                buy += "Copper";
+            }
+            Console.WriteLine(buy);
+        }
+    }
+}
