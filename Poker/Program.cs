3d28e908bb12524dc911381a183bb2b733ab72eb
firefly599 <nikolaivalkamo1@gmail.com>
2021-12-15 10:59:13 +0000

    Poker Hand

---
+++ Poker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace poker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Poker Hand
+            string[] hand = Console.ReadLine().Split(" ");
+            string values = "A23456789TJQK";
+            int max = 0;
+            foreach (char value in values)
+            {
+                int temp = 0;
+                foreach (string card in hand)
+                {
+                    if (card[0] == value)
+                    {
+                        temp++;
+                    }
+                }
+                if (temp > max)
+                {
+                    max = temp;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
