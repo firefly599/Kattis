91dea0da3f722a6b9467b89ca47bf831494e6514
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 15:09:39 +0000

    Oddities

---
+++ Oddities/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oddities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Oddities
+            int tests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tests; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("{0} is even", number);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is odd", number);
+                }
+            }
+        }
+    }
+}
