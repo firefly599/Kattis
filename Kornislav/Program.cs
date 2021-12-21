43bbe368d25d46b241c613fe72eeba33e755e317
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 15:49:54 +0000

    Kornislav

---
+++ Kornislav/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kornislav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kornislav
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            Array.Sort(numbers);
+            Console.WriteLine(numbers[0]*numbers[2]);
+        }
+    }
+}
