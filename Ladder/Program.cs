61fd0f1c952cf6bf6e82c972c7ff7285bdcd1973
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 15:38:02 +0000

    Ladder

---
+++ Ladder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ladder
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            double angle = numbers[1] * (Math.PI / 180);
+            double dis = numbers[0] / Math.Sin(angle);
+            Console.WriteLine(Math.Ceiling(dis));
+        }
+    }
+}
