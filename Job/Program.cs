5ea016e3f440ba5da488ad8f10e9e7f479926bf8
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 15:54:57 +0000

    Job Expenses

---
+++ Job/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Job
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Job Expenses
+            int tests = int.Parse(Console.ReadLine());
+            if (tests != 0)
+            { 
+                int[] numbers = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int total = 0;
+                foreach (int value in numbers)
+                {
+                    if (value < 0)
+                    {
+                        total += (value * -1);
+                    }
+                }
+                Console.WriteLine(total);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
