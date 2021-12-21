76bc4d851fcfc80e89b703b5ff87f7d073f3619c
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 14:01:04 +0000

    Avion

---
+++ Avion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace avion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Avion
+            string reg;
+            string blimps = "";
+            for (int i = 1; i <= 5; i++)
+            {
+                reg = Console.ReadLine();
+                for (int j = 2; j < reg.Length; j++)
+                {
+                    if (reg[j] == 'I' && reg[j-1] == 'B' && reg[j-2] == 'F')
+                    {
+                        blimps += i.ToString() + " ";
+                    }  
+                }
+            }
+            if (blimps.Length != 0)
+            {
+                Console.WriteLine(blimps);
+            }
+            else
+            {
+                Console.WriteLine("HE GOT AWAY!");
+            }
+        }
+    }
+}
