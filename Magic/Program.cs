383054e453f8f9ed2881af5791805c88dec09168
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 15:28:14 +0000

    Magic Trick

---
+++ Magic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Magic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Magic Trick
+            string contains = "";
+            string input = Console.ReadLine();
+            bool pos = true;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!(contains.Contains(input[i])))
+                {
+                    contains += input[i];
+                }
+                else
+                {
+                    pos = false;
+                    break;
+                }
+            }
+            if (pos)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
