9e472484386f2ccb27cde0861ccd217e6060669a
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 16:32:56 +0000

    Grading

---
+++ Grading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Grading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Grading
+            int[] bounds = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int score = int.Parse(Console.ReadLine());
+            if (score >= bounds[0])
+            {
+                Console.WriteLine("A");
+            }
+            else if (score >= bounds[1])
+            {
+                Console.WriteLine("B");
+            }
+            else if (score >= bounds[2])
+            {
+                Console.WriteLine("C");
+            }
+            else if (score >= bounds[3])
+            {
+                Console.WriteLine("D");
+            }
+            else if (score >= bounds[4])
+            {
+                Console.WriteLine("E");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
---
+++ PhD/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PhD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Help a PhD candidate out!
+            string eqn;
+            int tests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tests; i++)
+            {
+                eqn = Console.ReadLine();
+                if (eqn == "P=NP")
+                {
+                    Console.WriteLine("skipped");
+                }
+                else
+                {
+                    int[] split = Array.ConvertAll(eqn.Split("+"), int.Parse);
+                    Console.WriteLine(split[0] + split[1]);
+                }
+            }
+        }
+    }
+}
