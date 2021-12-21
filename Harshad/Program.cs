5e1eb9d51aad86746c261d68e294006b98d14352
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 13:41:15 +0000

    Harshad Number

---
+++ Harshad/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Harshad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Harshad Number
+            int sum = 0;
+            int total = int.Parse(Console.ReadLine());
+            int temp;
+            while (true)
+            { 
+                sum = 0;
+                temp = total;
+                while (temp != 0)
+                {
+                    sum += temp % 10;
+                    temp /= 10;
+                }
+                if (total % sum == 0)
+                {
+                    Console.WriteLine(total);
+                    break;
+                }
+                else
+                {
+                    total++;
+                }
+            }
+        }
+    }
+}
