f525b330a867dd8ec1856711b9263a2ab11925c6
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 14:50:28 +0000

    SMIL

---
+++ SMIL/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SMIL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SMIL
+            string word = Console.ReadLine();
+            word += "holder";
+            List<int> places = new List<int>();
+            for (int i = 0; i <= word.Length - 3; i++)
+            {
+                if (word.Substring(i, 2) == ":)" || 
+                    word.Substring(i, 2) == ";)" ||
+                    word.Substring(i, 3) == ":-)" ||
+                    word.Substring(i, 3) == ";-)")
+                {
+                    places.Add(i);
+                }
+            }
+            foreach (int place in places)
+            {
+                Console.WriteLine(place);
+            }
+        }
+    }
+}
