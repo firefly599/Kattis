27d1c55650b382cb11bea636ff1c17cb10121d3e
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 14:50:20 +0000

    Piece of Cake!

---
+++ Pieces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pieces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Piece of Cake!
+            int[] sizes = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int piece1 = sizes[1] * sizes[2] * 4;
+            int piece2 = (sizes[0] - sizes[1]) * sizes[2] * 4;
+            int piece3 = sizes[1] * (sizes[0] - sizes[2]) * 4;
+            int piece4 = (sizes[0] - sizes[1]) * (sizes[0] - sizes[2]) * 4;
+            int large1 = Math.Max(piece1, piece2);
+            int large2 = Math.Max(piece3, piece4);
+            Console.WriteLine(Math.Max(large1, large2));
+        }
+    }
+}
