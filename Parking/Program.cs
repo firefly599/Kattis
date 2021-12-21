9520b143279a44a12707cc0f3eeacb71814b31be
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 15:09:46 +0000

    Parking

---
+++ Parking/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parking
+            int total;
+            int dis;
+            int last;
+            int[] places;
+            int tests = int.Parse(Console.ReadLine());
+            List<int> distances = new List<int>();
+            for (int i = 0; i < tests; i++)
+            {
+                dis = 0;
+                distances.Clear();
+                total = int.Parse(Console.ReadLine());
+                places = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                foreach (int place in places)
+                {
+                    distances.Add(place);
+                }
+                distances.Sort();
+                last = distances[0];
+                for (int j = 1; j < distances.Count; j++)
+                {
+                    dis += distances[j] - last;
+                    last = distances[j];
+                }
+                dis *= 2;
+                Console.WriteLine(dis);
+            }
+        }
+    }
+}
