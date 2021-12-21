f122ffccdf5c49e0d2f162ef358e9a124004f7f3
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 16:56:10 +0000

    Forced Choice

---
+++ Forced/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Forced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Forced Choice
+            int[] nps = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            bool keep;
+            for (int i = 0; i < nps[2]; i++)
+            {
+                keep = false;
+                int[] cards = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                for (int j = 1 ; j <= cards[0]; j++)
+                {
+                    if (cards[j] == nps[1])
+                    {
+                        keep = true;
+                    }
+                }
+                if (keep)
+                {
+                    Console.WriteLine("KEEP");
+                }
+                else
+                {
+                    Console.WriteLine("REMOVE");
+                }
+            }
+        }
+    }
+}
