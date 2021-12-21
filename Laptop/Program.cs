75961fb282b2e6d144ba7467e97c2f1084e24cf1
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 15:31:31 +0000

    Laptop Sticker

---
+++ Laptop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laptop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Laptop Sticker
+            int[] sizes = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            if (sizes[2] < sizes[0] - 1 && sizes[3] < sizes[1] - 1)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
