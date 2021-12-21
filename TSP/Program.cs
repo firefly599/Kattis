870e36a59feb6c15ac514a03084362b27d7fd0b0
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 14:50:44 +0000

    TSP

---
+++ TSP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Travelling Salesperson 2D
+            int points = int.Parse(Console.ReadLine());
+            float[][] coords = new float[points][];
+            for (int i = 0; i < points; i++)
+            {
+                coords[i] = Array.ConvertAll(Console.ReadLine().Split(" "), float.Parse);
+            }
+            foreach (float[] xy in coords)
+            {
+                Console.WriteLine(xy[0] +" "+ xy[1]);
+            }
+
+        }
+    }
+}
