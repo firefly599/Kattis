c9a6217910ed4042ab561be165e9d5583be433ae
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 13:25:43 +0000

    No Duplicates

---
+++ Duplicates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // No Duplicates
+            bool rep = false;
+            string[] line = Console.ReadLine().Split(" ");
+            for (int i = 0; i < line.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (line[i] == line[j])
+                    {
+                        rep = true;
+                        break;  
+                    }
+                }
+                if (rep)
+                {
+                    break;
+                }
+            }
+            if (rep)
+            {
+                Console.WriteLine("no");
+            }
+            else
+            {
+                Console.WriteLine("yes");
+            }
+        }
+    }
+}
