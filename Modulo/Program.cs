9d4a2224fedc4507c4247be3e62709d2604f1159
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 15:22:37 +0000

    Modulo

---
+++ Modulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Modulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modulo
+            int number;
+            List<int> distinct = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                if (!(distinct.Contains(number % 42)))
+                {
+                    distinct.Add(number % 42);
+                }
+            }
+            Console.WriteLine(distinct.Count);
+        }
+    }
+}
