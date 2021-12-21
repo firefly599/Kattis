f2d60c6c69aa364c904853982dedb79d393637dc
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 17:04:27 +0000

    Fifty Shades of Pink

---
+++ Pink/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Pink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fifty Shades of Pink
+            int tests = int.Parse(Console.ReadLine());
+            string word;
+            string[] key = {"pink", "rose"};
+            int train = 0;
+            for (int i = 0; i < tests; i++)
+            {
+                word = Console.ReadLine();
+                word = word.ToLower();
+                for (int j = 0; j < 2; j++)
+                {
+                    if (word.Contains(key[j]))
+                    {
+                        train++;
+                        break;
+                    }
+                }
+            }
+            if (train != 0)
+            {
+                Console.WriteLine(train);
+            }
+            else
+            {
+                Console.WriteLine("I must watch Star Wars with my daughter");
+            }
+        }
+    }
+}
