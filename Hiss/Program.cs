86c8019f74dd410fedbf65b9fe33d5fb0c14cbf0
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-21 13:32:45 +0000

    Hissing Microphone

---
+++ Hiss/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hiss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hissing Microphone
+            string word = Console.ReadLine();
+            int length = word.Length;
+            bool hiss = false;
+            for (int i = 1; i < length; i++)
+            {
+                if (word[i] == 's' && word[i-1] == 's')
+                {
+                    hiss = true;
+                    break;
+                }
+            }
+            if (hiss)
+            {
+                Console.WriteLine("hiss");
+            }
+            else
+            {
+                Console.WriteLine("no hiss");
+            }
+        }
+    }
+}
