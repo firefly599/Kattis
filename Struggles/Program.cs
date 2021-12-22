88de740ce490152e63dac1409f56dd91ae51b599
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-22 19:03:31 +0000

    Contest struggles

---
+++ Struggles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Struggles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Contest struggles
+            double[] questions = Array.ConvertAll(Console.ReadLine().Split(" "), double.Parse);
+            double[] percentage = Array.ConvertAll(Console.ReadLine().Split(" "), double.Parse);
+            double remaining = ((questions[0] * percentage[0]) - (questions[1] * percentage[1])) / (questions[0] - questions[1]);
+            if (remaining > 100 || remaining < 0)
+            {
+                Console.WriteLine("impossible");
+            }
+            else
+            {
+                Console.WriteLine(remaining);
+            }
+        }
+    }
+}
