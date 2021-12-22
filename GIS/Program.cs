e29a992577bf1d19acd042f2cbb5e611ffec6622
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-22 14:09:20 +0000

    Greedily Increasing Subsequence

---
+++ GIS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greedily Increasing Subsequence
+            int numbers = int.Parse(Console.ReadLine());
+            int[] values = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            List<int> sub = new List<int>();
+            sub.Add(values[0]);
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i] > sub[sub.Count - 1])
+                {
+                    sub.Add(values[i]);
+                }
+            }
+            Console.WriteLine(sub.Count);
+            foreach (int value in sub)
+            {
+                Console.Write("{0} ", value);
+            }
+        }
+    }
+}
