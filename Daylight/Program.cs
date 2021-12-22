198fd10a27824086e0aec775ddf215ef6b0f1e11
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-22 21:56:37 +0000

    Saving Daylight

---
+++ Daylight/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Daylight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Saving Daylight
+            string line;
+            string[] start;
+            string[] end;
+            string[] split;
+            while (!String.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                split = line.Split(" ");
+                start = split[3].Split(":");
+                end = split[4].Split(":");
+
+                int hours = int.Parse(end[0]) - int.Parse(start[0]);
+                int minutes = int.Parse(end[1]) - int.Parse(start[1]);
+                if (minutes < 0)
+                {
+                    hours--;
+                    minutes = 60 + minutes;
+                }
+                Console.WriteLine("{0} {1} {2} {3} hours {4} minutes", split[0], split[1], split[2], hours, minutes);
+            }
+        }
+    }
+}
