270f69742abec7bec9e01558f059bb0eaf55b9a8
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-22 20:23:01 +0000

    Train Passengers

---
+++ Train/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Train Passengers
+            int[] info = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int[] station;
+            int current = 0;
+            int capacity = info[0];
+            bool possible = true;
+            for (int i = 0; i < info[1]; i++)
+            {
+                station = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                current -= station[0];
+                if (current < 0)
+                {
+                    possible = false;
+                    break;
+                }
+                current += station[1];
+                if (current > capacity || (capacity - current > 0 && station[2] > 0) )
+                {
+                    possible = false;
+                    break;
+                }
+            }
+            if (current > 0)
+            {
+                possible = false;
+            }
+            if (possible)
+            {
+                Console.WriteLine("possible");
+            }
+            else
+            {
+                Console.WriteLine("impossible");
+            }
+        }
+    }
+}
