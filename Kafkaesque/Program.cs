c0c707c92e87ca74a36d5c14c385387fad7bdb20
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-22 17:55:23 +0000

    Kafkaesque

---
+++ Kafkaesque/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kafkaesque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kafkaesque
+            int stamps = int.Parse(Console.ReadLine());
+            int passes = 1;
+            int[] numbers = new int[stamps];
+            for (int i = 0; i < stamps; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 1; i < stamps; i++)
+            {
+                if (numbers[i] < numbers[i - 1])
+                {
+                    passes++;
+                }
+            }
+            Console.WriteLine(passes);
+        }
+    }
+}
