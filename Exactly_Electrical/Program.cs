441f918ac5676e31a3607763b9cbc022261dd38f
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-22 16:25:16 +0000

    Exactly Electrical

---
+++ Exactly_Electrical/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exactly_Electrical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exactly Electrical
+            int[] start = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int[] end = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int power = int.Parse(Console.ReadLine());
+            int x = Math.Abs(start[0] - end[0]);
+            int y = Math.Abs(start[1] - end[1]);
+            int spent = power - x - y;
+            if (spent % 2 == 0  && spent >= 0)
+            {
+                Console.WriteLine("Y");
+            }
+            else
+            {
+                Console.WriteLine("N");
+            }
+        }
+    }
+}
