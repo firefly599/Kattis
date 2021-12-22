05ee7291075dd402cec677d3d3c6f6754cadf706
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-22 17:21:30 +0000

    Test file

---
+++ 0_Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _0_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] temp = new int[] {1, 2};
+            int[] target = new int[] {2, 1};
+            if (Enumerable.SequenceEqual(temp, target))
+            {
+                Console.WriteLine("here");
+            }
+        }
+    }
+}
