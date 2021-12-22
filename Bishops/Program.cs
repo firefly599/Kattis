aef1366d853b5e554ad7ab02a58873b4dc6c9621
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-22 23:36:36 +0000

    Bishops

---
+++ Bishops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bishops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string input;
+            while (!string.IsNullOrEmpty(input = Console.ReadLine()))
+            {
+                num = int.Parse(input);
+                if (num > 1)
+                {
+                    Console.WriteLine(num + num - 2);
+                }
+                else
+                {
+                    Console.WriteLine("1");
+                }
+            }
+        }
+    }
+}
