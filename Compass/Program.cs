73ad9b3950ac6c655d07423aafcaeec1b7902a22
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-22 18:11:07 +0000

    Jumbled Compass - Failed

---
+++ Compass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Compass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Jumbled Compass
+            int current = int.Parse(Console.ReadLine());
+            int target = int.Parse(Console.ReadLine());
+            int difference = target - current;
+            if (Math.Abs(difference) > 180)
+            {
+                if (difference > 0)
+                {
+                    difference = - (360 - Math.Abs(difference));
+                }
+                else
+                {
+                    difference = 360 - Math.Abs(difference);
+                }
+            }
+            Console.WriteLine(difference);
+        }
+    }
+}
