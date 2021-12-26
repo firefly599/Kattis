9f096b4aa85c9faa5dbb7908f1a50ad1ce4bee22
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-26 22:23:31 +0000

    Knot Knowledge

---
+++ knot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Knot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Knot Knowledge
+            int knots = int.Parse(Console.ReadLine());
+            int[] required = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int[] learnt = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            foreach (int knot in required)
+            {
+                if (!(learnt.Contains(knot)))
+                {
+                    Console.WriteLine(knot);
+                    break;
+                }
+            }
+        }
+    }
+}
