96e00b9409d3fca3d3fbf92bdc8c5b21100bf5ce
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-26 17:18:38 +0000

    Building Pyramids

---
+++ Pyramids/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pyramids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Building Pyramids
+            double total = 0;
+            int layer = 1;
+            int output = 0;
+            int input = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                total += Math.Pow(layer, 2);
+                if (total <= input)
+                {
+                    layer += 2;
+                    output++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
