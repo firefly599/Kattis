06fba44fd2b2451a1f0ee4282a7ba3c850ddc5fa
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-26 17:27:23 +0000

    Simon says

---
+++ Simon/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Simon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simon says
+            string[] line;
+            int tests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tests; i++)
+            {
+                line = Console.ReadLine().Split();
+                if (line[0] == "Simon" && line[1] == "says")
+                {
+                    for (int j = 2; j < line.Length; j++)
+                    {
+                        Console.Write("{0} ", line[j]);
+                    }
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
