bb7dc540797024ad257f6f9df1357ace8ac85bb9
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-26 22:34:40 +0000

    Judging Moose

---
+++ Moose/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Moose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Judging Moose
+            int[] tinds = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            if (tinds[0] == tinds[1] && Math.Max(tinds[0], tinds[1]) > 0)
+            {
+                Console.WriteLine("Even {0}", tinds[0]*2);
+            }
+            else if (tinds[0] == 0 && tinds[1] == 0)
+            {
+                Console.WriteLine("Not a moose");
+            }
+            else
+            {
+                Console.WriteLine("Odd {0}", Math.Max(tinds[0], tinds[1]) * 2);
+            }
+        }
+    }
+}
