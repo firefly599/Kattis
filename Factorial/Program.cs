bff78e15d1b7d5d40ba39ddfdcc43dbb3ea62b06
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-26 22:13:05 +0000

    Last Factorial Digit

---
+++ Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Last Factorial Digit
+            int tests = int.Parse(Console.ReadLine());
+            int input;
+            double total;
+            string strVersion;
+            for (int i = 0; i < tests; i++)
+            {
+                total = 1;
+                input = int.Parse(Console.ReadLine());
+                for (int j = 1; j <= input; j++)
+                {
+                    total *= j;
+                }
+                strVersion = total.ToString();
+                Console.WriteLine(strVersion[strVersion.Length - 1]);
+            }
+        }
+    }
+}
