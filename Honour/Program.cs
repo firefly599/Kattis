9c0a8075a8708e35c9c4ceec86ab3727734d5339
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-26 23:07:42 +0000

    Honour Thy (Apaxian) Parent

---
+++ Honour/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Honour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Honour Thy (Apaxian) Parent
+            string[] input = Console.ReadLine().Split(" ");
+            string vowels = "aiou";
+            string full;
+            string substring;
+            if (input[0][input[0].Length - 1] == 'e')
+            {
+                full = input[0] + "x" + input[1];
+            }
+            else if (input[0].Substring(input[0].Length - 2, 2) == "ex")
+            {
+                full = input[0] + input[1];
+            }
+            else if (vowels.Contains(input[0][input[0].Length - 1]))
+            {
+                substring = input[0].Substring(0, input[0].Length - 1);
+                full = substring + "ex" + input[1];
+            }
+            else
+            {
+                full = input[0] + "ex" + input[1];
+            }
+            Console.WriteLine(full);
+        }
+    }
+}
