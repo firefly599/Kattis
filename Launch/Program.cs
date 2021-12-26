324a3a7b05a4e634d28e0ca3445cd8bc4f572c8a
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-26 22:04:09 +0000

    License to Launch

---
+++ Launch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Launch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // License to Launch
+            int tests = int.Parse(Console.ReadLine());
+            int[] junk = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            double lowest = 9999999999;
+            int day = 0;
+            for (int i = 0; i < junk.Length; i++)
+            {
+                if (junk[i] < lowest)
+                {
+                    day = i;
+                    lowest = junk[i];
+                }
+            }
+            Console.WriteLine(day);
+        }
+    }
+}
