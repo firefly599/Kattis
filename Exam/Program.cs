ece6e43fa20f0ac759c4920fcb67102b4cc2716d
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-27 20:01:40 +0000

    Final Exam

---
+++ Exam/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Final Exam
+            int total = 0;
+            int tests = int.Parse(Console.ReadLine());
+            string next;
+            string current = Console.ReadLine();
+            for (int i = 0; i < tests; i++)
+            {
+                next = Console.ReadLine();
+                if (current == next)
+                {
+                    total ++;
+                }
+                current = next;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
