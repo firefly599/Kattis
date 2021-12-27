1e796d1fc5298fe0f517dd7c05e9ace57197d679
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-27 21:24:09 +0000

    Drunk Vigenère

---
+++ Circle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Estimating the Area of a Circle
+            double totalArea;
+            double estimatedArea;
+            double[] values;
+            values = Array.ConvertAll(Console.ReadLine().Split(" "), double.Parse);
+            while (values[0] != 0 && values[1] != 0 && values[2] != 0)
+            {
+                totalArea = Math.Pow(values[0], 2) * Math.PI;
+                estimatedArea = 4 * (values[2] / values[1]) * Math.Pow(values[0], 2);
+                Console.WriteLine("{0} {1}", totalArea, estimatedArea);
+                values = Array.ConvertAll(Console.ReadLine().Split(" "), double.Parse);
+            } 
+        }
+    }
+}
---
+++ Drunk/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Drunk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Drunk Vigenère
+            int encryptedValue;
+            int keyValue;
+            int decryptedValue;
+            string encrypted = Console.ReadLine();
+            string key = Console.ReadLine();
+            string decrypted = "";
+            for (int i = 0; i < encrypted.Length; i++)
+            {
+                encryptedValue = (int)encrypted[i];
+                keyValue = (int)key[i];
+                if ((i+1) % 2 == 0)
+                {
+                    decryptedValue = encryptedValue + (keyValue - 65);
+                    if (decryptedValue > 90)
+                    {
+                        decryptedValue = 65 + (decryptedValue % 91);
+                    }
+                
+                }
+                else
+                {
+                    decryptedValue = encryptedValue - (keyValue - 65);
+                    if (decryptedValue < 65)
+                    {
+                        decryptedValue = 90 - (64 - decryptedValue);
+                    }
+                }
+                decrypted += (char)decryptedValue;
+                
+            }
+            Console.WriteLine(decrypted);
+        }
+    }
+}
