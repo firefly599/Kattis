a832719e190d6ff95ab3fa9e66dd3303301b7948
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-28 20:38:08 +0000

    Sum of the Others

---
+++ Sum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sum of the Others
+            List<string> temp = new List<string>();
+            List<string> original = new List<string>();
+            string[] input;
+            string line;
+            int total;
+            int target;
+            string current;
+            while (!string.IsNullOrEmpty((line = Console.ReadLine())))
+            {
+                original.Clear();
+                input = line.Split(" ");
+                foreach (string item in input)
+                {
+                    original.Add(item);
+                }
+                
+                for (int i = 0; i < original.Count; i++)
+                {
+                    
+                    temp.Clear();
+                    foreach (string obj in original)
+                    {
+                        temp.Add(obj);
+                    }
+
+                    target = int.Parse(temp[i]);
+                    temp.RemoveAt(i);
+                    total = 0;
+                    for (int j = 0; j < temp.Count; j++)
+                    {
+                        current = temp[j];
+                        if (temp[j][0] == '-')
+                        {
+                            current = temp[j].Substring(1);
+                            total -= int.Parse(current);
+                        }
+                        else
+                        {
+                            total += int.Parse(current);
+                        }
+                    
+                    }
+                    if (total == target)
+                    {
+                        Console.WriteLine(total);
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+}
