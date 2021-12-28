f3dfdc03df14f0894d856f8a635b25d3d476d969
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2021-12-28 15:48:50 +0000

    Bijele

---
+++ Bijele/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bijele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bijele
+            int[] required = new int[] { 1, 1, 2, 2, 2, 8};
+            int[] actual = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int[] altered = new int [required.Length];
+            for (int i = 0; i < required.Length; i++)
+            {
+                if (actual[i] != required[i])
+                {
+                    altered[i] = required[i] - actual[i]; 
+                }
+            }
+            foreach (int amount in altered)
+            {
+                Console.Write("{0} ", amount);
+            }
+        }
+    }
+}
