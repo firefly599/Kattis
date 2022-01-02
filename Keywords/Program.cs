41d8cae5ba9da2507a37e3584f9d6a674cd27eaa
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2022-01-02 13:38:04 +0000

    Keywords

---
+++ Keywords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Keywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Keywords
+            HashSet<string> unique = new HashSet<string>();
+            int words = int.Parse(Console.ReadLine());
+            string input;
+            for (int i = 0; i < words; i++)
+            {
+                input = Console.ReadLine();
+                input = input.ToLower();
+                while (input.Contains('-'))
+                {
+                    input = input.Replace('-', ' ');
+                }
+
+                if (!unique.Contains(input))
+                {
+                    unique.Add(input);
+                }
+            }
+            Console.WriteLine(unique.Count);
+        }
+    }
+}
