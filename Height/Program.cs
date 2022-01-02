35ed15b8448a78865cb3dd58f58f5d277767f98f
NIKOLAI VALKAMO <nikolaivalkamo1@gmail.com>
2022-01-02 13:04:59 +0000

    Simon Says

---
+++ Height/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Height
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Height ordering
+            int tests = int.Parse(Console.ReadLine());
+            int[] heights;
+            int steps;
+            int temp;
+            for (int x = 0; x < tests; x++)
+            {
+                steps = 0;
+                heights = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                for (int i = 1; i < heights.Length; i++)
+                {
+                    for (int j = 1; j < i; j++)
+                    {
+                        if (heights[i] < heights[j])
+                        {
+                            temp = heights[j];
+                            heights[i] = heights[j];
+                            heights[j] = temp;
+                            steps += Math.Abs(i - j);
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine("{0} {1}", heights[0], steps);
+            }
+        }
+    }
+}
---
+++ Says/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Says
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simon says
+            int tests = int.Parse(Console.ReadLine());
+            int length;
+            string input;
+            string[] line;
+            string output;
+            for (int i = 0; i < tests; i++)
+            {
+                input = Console.ReadLine();
+                line = input.Split(" ");
+                output = "";
+                if (line.Length > 1)
+                {
+                    if (line[0] == "simon" && line[1] == "says")
+                    {
+                        length = input.Length;
+                        if (length > 10)
+                        {
+                            output = input.Substring(11, length-11);
+                        }
+                    }
+                }
+                
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
