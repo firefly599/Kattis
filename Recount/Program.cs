7fba69aecef3ab87ea024ad264489569fc97a5df
firefly499 <nikolaivalkamo1@gmail.com>
2022-01-14 13:43:49 +0000

    Recount - Time limit exceeded

---
+++ Recount/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Recount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Recount
+            string line;
+            List<string> names = new List<string>();
+            List<int> votes = new List<int>();
+            int index = 0;
+            while ((line = Console.ReadLine()) != "***")
+            {
+                if (names.Contains(line))
+                {
+                    index = names.IndexOf(line);
+                    votes[index] += 1;
+                }
+                else
+                {
+                    names.Add(line);
+                    votes.Add(1);
+                }
+            }
+            int max = votes.Max();
+            int first = votes.FindIndex(0, votes.Count, x => x==max);
+            int second = votes.FindIndex(first+1, votes.Count - first - 1, x => x==max);
+
+            if (second == -1)
+            {
+                Console.WriteLine(names[first]);
+            }
+            else
+            {
+                Console.WriteLine("Runoff!");
+            }
+
+        }
+    }
+}
