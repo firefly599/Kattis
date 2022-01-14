029483436e37758a6517332bd6c627a0a5d2636c
firefly499 <nikolaivalkamo1@gmail.com>
2022-01-14 12:57:15 +0000

    Simple Addition

---
+++ Addition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple Addition
+            string no1 = Console.ReadLine();
+            string no2 = Console.ReadLine();
+            BigInteger sum = BigInteger.Parse(no1) + BigInteger.Parse(no2);
+            Console.WriteLine(sum);
+        }
+    }
+}
