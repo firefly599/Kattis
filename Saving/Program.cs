8e6c6cb08100b91fccd72c1a6955a93595471624
firefly499 <nikolaivalkamo1@gmail.com>
2022-01-17 13:37:46 +0000

    Saving for retirement

---
+++ Saving/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Saving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Saving for retirement
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int bob = (input[1] - input[0]) * input[2];
+            int alice = 0;
+            int age = input[3];
+            while (alice <= bob)
+            {
+                alice += input[4];
+                age++;
+            }
+            Console.WriteLine(age);
+        }
+    }
+}
