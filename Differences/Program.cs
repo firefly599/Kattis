b4160d6031f16e7b9be3fbdaf420c945e0fbff31
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-04 17:40:17 +0000

    Detailed Differences

---
+++ Differences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Differences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Detailed Differences
+            int test = int.Parse(Console.ReadLine());
+            string first;
+            string second;
+            string simular;
+            for (int t = 0; t < test; t++)
+            {
+                first = Console.ReadLine();
+                second = Console.ReadLine();
+                simular = "";
+                for (int i = 0; i < first.Length; i++)
+                {
+                    if (first[i] == second[i])
+                    {
+                        simular += ".";
+                    }
+                    else
+                    {
+                        simular += "*";
+                    }
+                }
+                Console.WriteLine(first);
+                Console.WriteLine(second);
+                Console.WriteLine(simular);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
