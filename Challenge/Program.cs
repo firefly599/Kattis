01b930bb440d9f0c5af5e902576dd2c76d0bfae9
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-04 18:06:40 +0000

    A Real Challenge

---
+++ Challenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A Real Challenge
+            double area = double.Parse(Console.ReadLine());
+            double length = Math.Sqrt(area);
+            double perm = 4 * length;
+            Console.WriteLine(perm);
+        }
+    }
+}
