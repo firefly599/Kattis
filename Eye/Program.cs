3e87f93b5e25eb4f3ad9d3d2c5cceafe14c633a8
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-04 16:41:53 +0000

    Eye of Sauron

---
+++ Eye/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Eye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Eye of Sauron
+            string input = Console.ReadLine();
+            int left = 0;
+            int right = 0;
+            bool rightSide = false;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '|' && !(rightSide))
+                {
+                    left++;
+                }
+                else if (input[i] == '|' && (rightSide))
+                {
+                    right++;
+                }
+
+                if (input[i] == ')')
+                {
+                    rightSide = true;
+                }
+            }
+            if (left == right)
+            {
+                Console.WriteLine("correct");
+            }
+            else
+            {
+                Console.WriteLine("fix");
+            }
+        }
+    }
+}
