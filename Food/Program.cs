e3af1c6e141a13fb8beb011b2367d9abb916c82b
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-04 16:32:25 +0000

    Unfinished Dont know how to calculate

---
+++ Food/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Free Food
+            int amount = int.Parse(Console.ReadLine());
+            int overlap = 0;
+            int[] times;
+            List<int[]> days = new List<int[]>();
+            for (int i = 0; i < amount; i++)
+            {
+                times = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                days.Add(times);
+            }
+           
+        }
+    }
+}
