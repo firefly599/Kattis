5b2cb47bdd7d19c92f9fb43a7cf15174c3ff36a3
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-04 17:52:03 +0000

    Alphabet Spam

---
+++ Spam/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Spam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Alphabet Spam
+            char blank = '_';
+            string input = Console.ReadLine();
+            double length = input.Length;
+            double blanks = 0;
+            double upper = 0;
+            double lower = 0;
+            double symbol = 0;
+
+            char current;
+            for (int i = 0; i < length; i++)
+            {
+                current = input[i];
+                if (char.IsUpper(current))
+                {
+                    upper++;
+                }
+                else if (char.IsLower(current))
+                {
+                    lower++;
+                }
+                else if (current == blank)
+                {
+                    blanks++;
+                }
+                else
+                {
+                    symbol++;
+                }
+            }
+
+            Console.WriteLine(blanks / length);
+            Console.WriteLine(lower / length);
+            Console.WriteLine(upper / length);
+            Console.WriteLine(symbol / length);
+        }
+    }
+}
