b1fb1c602b3a55f9ea8d68a4e40659fdc0ff79c5
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-14 13:55:17 +0000

    Starting Watchdog

---
+++ Watchdog/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Watchdog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Watchdog
+            int tests = int.Parse(Console.ReadLine());
+            int[] roof;
+            int[] coords;
+            for (int test = 0; test < tests; test++)
+            {
+                roof = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                for (int i = 0; i < roof[1]; i++)
+                {
+                    coords = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                    for (int x = 0; x < roof[0]; x++)
+                    {
+                        for (int y = 0; y < roof[0]; y++)
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
