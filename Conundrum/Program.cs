f35ebeb143de2a2090a96ef5ba8b7599f9df6916
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-16 15:49:28 +0000

    Cryptographer's Conundrum

---
+++ Conundrum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Conundrum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cryptographer's Conundrum
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            int counter = 0;
+            string per = "per";
+            for (int i = 0; i < input.Length-2; i += 3)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (!(input[i+j] == per[j]))
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
