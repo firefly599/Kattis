bae8b6355d665887a030e4d0e5b4fac7356090e7
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-16 15:40:28 +0000

    Datum

---
+++ Datum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Datum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Datum
+            int[] date = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            DateTime day = new DateTime(2009, date[1], date[0]);
+            Console.WriteLine(day.DayOfWeek);
+        }
+    }
+}
