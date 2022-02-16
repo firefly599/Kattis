3dacd8db12ef93d6351313c72fa9c5d26be05fde
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-16 16:39:08 +0000

    The Easiest Problem Is This One

---
+++ Easiest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Easiest
+{
+    class Program
+    {
+        static int SumDigits(long number)
+        {
+            long temp = number;
+            long sum = 0;
+            while (temp != 0)
+            {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            return (int)sum;
+        }
+        static void Main(string[] args)
+        {
+            // The Easiest Problem Is This One
+            long np;
+            long input;
+            int p;
+            int nSize;
+            int pSize;
+            while (true)
+            {
+                input = long.Parse(Console.ReadLine());
+                if (input == 0)
+                {
+                    break;
+                }
+                p = 11;
+                nSize = SumDigits(input);
+
+                while (true)
+                {
+                    np = input * p;
+                    pSize = SumDigits(np);
+                    if (pSize == nSize)
+                    {
+                        Console.WriteLine(p);
+                        break;
+                    }
+                    p++;
+                }
+            }
+        }
+    }
+}
