904c53119bcb11d3f4995506c837ca1ff1ad0137
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-16 15:15:24 +0000

    Finding An A

---
+++ A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Finding An A
+            string input = Console.ReadLine();
+            string copy = input.ToLower();
+            int index = copy.IndexOf("a");
+
+            string output = input.Substring(index, input.Length - index);
+            Console.WriteLine(output);
+        }
+    }
+}
