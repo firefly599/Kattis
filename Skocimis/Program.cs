952356bf760b9c54716c009584d656d5501a5eb5
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-18 15:27:10 +0000

    Skocimis

---
+++ Skocimis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Skocimis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skocimis
+            int[] values = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int left = values[1] - values[0]; 
+            int right = values[2] - values[1];
+            int max = Math.Max(left, right);
+            Console.WriteLine(max - 1);
+        }
+    }
+}
