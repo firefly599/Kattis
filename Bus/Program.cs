8d5975b0317d7f4ba273ebeab78366585a31f4b3
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-18 16:38:03 +0000

    Bus idk math doesnt work in my head anymore

---
+++ Bus/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bus
+            int tests = int.Parse(Console.ReadLine());
+            int stops;
+            double people;
+            for (int i = 0; i < tests; i++)
+            {
+                stops = int.Parse(Console.ReadLine());
+                people = 0;
+                for (int j = 0; j < stops; j++)
+                {
+                    people += 0.5;
+                    people += (people / 2);
+                }
+                Console.WriteLine(people);
+                Console.WriteLine((int)people);
+            }
+        }
+    }
+}
+
