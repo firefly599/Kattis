9b2fe4f72a9a16844c1f0c3c1a511962128a8b90
firefly599 <nikolaivalkamo1@gmail.com>
2022-02-18 14:43:14 +0000

    Reverse

---
+++ Reverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reverse
+            int amount = int.Parse(Console.ReadLine());
+            double input;
+            double[] array = new double[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                input = double.Parse(Console.ReadLine());
+                array[i] = input;
+            }
+
+            for (int i = 0; i < amount; i++)
+            {
+                Console.WriteLine(array[amount-i-1]);
+            }
+        }
+    }
+}
