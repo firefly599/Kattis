f247b051c3a9f48e7fb4a866b1c33d8a5f8fdbd8
firefly499 <nikolaivalkamo1@gmail.com>
2022-02-27 00:10:42 +0000

    Aaah!

---
+++ Ah/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aaah!
+            string firt = Console.ReadLine();
+            string second = Console.ReadLine();
+            if (firt.Length < second.Length)
+            {
+                Console.WriteLine("no");
+            }
+            else
+            {
+                Console.WriteLine("go");
+            }
+        }
+    }
+}
