3264c4ea1d981633fc4108d16ead421954904967
firefly499 <nikolaivalkamo1@gmail.com>
2022-06-01 15:12:20 +0100

    CPR Number

---
+++ Cpr_Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cpr_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CPR Number
+            string multiple = "4327654321";
+            string input = Console.ReadLine();
+            input = input.Remove(6, 1);
+            int total = 0;
+            int temp;
+            for (int i = 0; i < 10; i++)
+            {
+                temp = ((int)multiple[i] - '0') * ((int)input[i] - '0');
+                total += temp;
+            }
+            if (total % 11 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
