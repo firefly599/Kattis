09000a98cc896bc3499958e11b1e536e3cfae0a7
firefly499 <nikolaivalkamo1@gmail.com>
2022-06-01 15:28:55 +0100

    Mirror Images

---
+++ Mirror/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace mirror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mirror Images
+            int tests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tests; i++)
+            {
+                int[] sizes = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                string[] lines = new string[sizes[0]];
+                Console.WriteLine("Test {0}", i+1);
+                for (int j = 0; j < sizes[0]; j++)
+                {
+                    lines[j] = Console.ReadLine();
+                }
+
+                for (int j = lines.Length-1; j >= 0; j--)
+                {
+                    char[] array = lines[j].ToCharArray();
+                    Array.Reverse(array);
+                    Console.WriteLine(new string (array));
+                    
+                }
+            }
+        }
+    }
+}
